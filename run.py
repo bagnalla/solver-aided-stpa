f989f190c02ae351bc4fbcc6780bcb8ce155fd50
Alex <ab667712@ohio.edu>
2023-08-17 19:13:09 -0400

    Add comments to example system

--- run.py
+++ run.py
@@ -25,39 +25,79 @@ from yices import Config, Context, Model, Status, Types, Terms
 # actual system below.
 
 # Test system.
-sys: System = System(name = 'system',
-                     types = [FinTypeDecl(name = 'DryOrWet', elements = ['dry', 'wet'])],
-                     vars = [],
-                     actions = [],
-                     
-                     components =
-                     [System(name = 'aircraft',
-                             types = [],
-                             vars = [VarDecl('landing', 'bool')],
-                             components = [],
-                             actions = [Action(name = 'hit_brakes',
-                                               constraints =
-                                               [Ident.ofStr('system.aircraft.landing')])],
-                             invariants = []),
-                      System(name = 'environment',
-                             types = [],
-                             vars = [VarDecl('runway_status',
-                                             Ident.ofStr('system.DryOrWet'))],
-                             components = [],
-                             actions = [],
-                             invariants = ['true']),
-                      System(name = 'wheels',
-                             types = [],
-                             vars = [VarDecl('weight_on_wheels', 'bool')],
-                             components = [],
-                             actions = [],
-                             invariants = [])],
-                     
-                     invariants =
-                     [when(conj([Ident.ofStr('system.aircraft.landing'),
-                                 eq(Ident.ofStr('system.environment.runway_status'),
-                                    Ident.ofStr('system.dry'))]),
-                           Ident.ofStr('system.wheels.weight_on_wheels'))])
+sys: System = \
+    System(name = 'sys',
+           
+           # Declare a 'FinType' (finite type, or enum) named
+           # 'sys.DryOrWet' and has with elements 'sys.dry' and 'sys.wet'.
+           types = [FinTypeDecl(name = 'DryOrWet', elements = ['dry', 'wet'])],
+           
+           # No variables associated with the 'sys' system.
+           vars = [],
+           
+           # No actions associated with the 'sys' system.
+           actions = [],
+           
+           # The 'sys' system has three components: 'sys.aircraft',
+           # 'sys.environment', and 'sys.wheels' (perhaps the wheels
+           # should be a component of the aircraft instead).
+           components =
+           
+           # 'sys.aircraft' system.
+           [System(name = 'aircraft',
+                   types = [],
+                   
+                   # The 'aircraft' component has a single Boolean
+                   # variable called 'sys.aircraft.landing' denoting
+                   # whether the aircraft is currently landing.
+                   vars = [VarDecl('landing', 'bool')],
+                   components = [],
+                   
+                   # The 'aircraft' component has a single action
+                   # called 'aircraft.hit_brakes' with a single safety constraint.
+                   actions = [Action(name = 'hit_brakes',
+                                     
+                                     # Variable 'sys.aircraft.landing' must be true
+                                     # for the 'hit_brakes' action to be safe.
+                                     constraints = [Ident.ofStr('sys.aircraft.landing')])],
+                   
+                   # No internal invariant for 'sys.aircraft'.
+                   invariants = []),
+            
+            # 'sys.environment' system.
+            System(name = 'environment',
+                   types = [],
+                   
+                   # The environment has a single variable
+                   # 'sys.environment.runway_status' with type 'sys.DryOrWet'.
+                   vars = [VarDecl('runway_status', Ident.ofStr('sys.DryOrWet'))],
+                   components = [],
+                   actions = [],
+                   
+                   # Trivial invariant that is always true (equivalent
+                   # to no invariant at all).
+                   invariants = ['true']),
+            
+            # 'sys.wheels' system.
+            System(name = 'wheels',
+                   types = [],
+                   vars = [VarDecl('weight_on_wheels', 'bool')],
+                   components = [],
+                   actions = [],
+                   invariants = [])],
+           
+           # The 'sys' system has a single invariant:
+           # 'when sys.aircraft.landing and
+           # sys.environment.runway_status = sys.dry', sys.wheels.weight_on_wheels'
+           
+           # In prose: When the aircraft is landing and the runway is
+           # dry, weight_on_wheels is true. This is an assumption we
+           # have about the world.
+           invariants =
+           [when(conj([Ident.ofStr('sys.aircraft.landing'),
+                       eq(Ident.ofStr('sys.environment.runway_status'),
+                          Ident.ofStr('sys.dry'))]),
+                 Ident.ofStr('sys.wheels.weight_on_wheels'))])
 
 # UCA hit_brakes:
 #   typ: issued
@@ -66,13 +106,22 @@ sys: System = System(name = 'system',
 #   typ: not issued
 #   context: aircraft.landing
 
+    # Action 'sys.aircraft.hit_brakes' is potentially hazardous when
+    # issued when 'sys.wheels.weight_on_wheels' is false.
+
 # Test UCAs.
-ucas: List[UCA] = [UCA(action = Ident.ofList(['system', 'aircraft', 'hit_brakes']),
-                       type = 'issued',
-                       context = neg(Ident.ofList(['system', 'wheels', 'weight_on_wheels']))),
-                   UCA(action = Ident.ofList(['system', 'aircraft', 'hit_brakes']),
-                       type = 'not issued',
-                       context = Ident.ofList(['system', 'aircraft', 'landing']))]
+ucas: List[UCA] = \
+    [UCA(action = Ident.ofStr('sys.aircraft.hit_brakes'),
+         type = 'issued',
+         # Action 'sys.aircraft.hit_brakes' is potentially hazardous
+         # when issued when 'sys.wheels.weight_on_wheels' is false.
+         context = neg(Ident.ofStr('sys.wheels.weight_on_wheels'))),
+     
+     UCA(action = Ident.ofStr('sys.aircraft.hit_brakes'),
+         type = 'not issued',
+         # Action 'sys.aircraft.hit_brakes' is potentially hazardous when
+         # NOT issued when 'sys.aircraft.landing' is true.
+         context = Ident.ofStr('sys.aircraft.landing'))]
 
 # Check that the system is well-formed.
 try:
